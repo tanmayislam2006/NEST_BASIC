f76467c2b8b81f2c9590a8421b9beef06bc2a826
tanmay2006 <tanmayislam101@gmail.com>
2025-12-09 18:47:02 +0600

    create a new module and practice the curd

---
+++ src/users/users.controller.ts
@@ -0,0 +1,38 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  getAllUsers() {
+    return this.usersService.getAllUsers();
+  }
+  @Get(':userId')
+  getUserById(@Param('userId') id: string) {
+    return this.usersService.getUserById(id);
+  }
+  @Post()
+  createUser(@Body() userInfo: Record<string, any>) {
+    return this.usersService.createUser(userInfo);
+  }
+  @Put(':userId')
+  updateUser(
+    @Body() updateInfo: Record<string, any>,
+    @Param('userId') id: string,
+  ) {
+    return this.usersService.updateUser(id, updateInfo);
+  }
+  @Delete(':userId')
+  deleteUser(@Param('id') id: string) {
+    return this.usersService.deleteUser(id);
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+export interface User {
+  id: string;
+  [key: string]: any;
+}
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+  getAllUsers() {
+    return {
+      success: true,
+      message: 'the url hit the url /users',
+      data: this.users,
+    };
+  }
+  createUser(userInfo: Record<string, any>) {
+    const newUser = {
+      id: Date.now().toString(),
+      ...userInfo,
+    };
+    this.users.push(newUser);
+    return {
+      success: true,
+      message: 'user created successfully',
+      data: newUser,
+    };
+  }
+  getUserById(id: string) {
+    const result = this.users.find((u) => u.id === id);
+
+    return {
+      success: true,
+      message: 'User Info Get Successfully',
+      data: result,
+    };
+  }
+  updateUser(id: string, updateData: Record<string, any>) {
+    const user = this.users.find((u) => u.id === id);
+    if (!user) throw new Error('User Not Found');
+    Object.assign(user, updateData);
+    return {
+      success: true,
+      massage: 'user updated ',
+      data: user,
+    };
+  }
+  deleteUser(id: string) {
+    this.users = this.users.filter((u) => u.id !== id);
+    return {
+      success: true,
+      massage: 'user delete success fully',
+    };
+  }
+}
